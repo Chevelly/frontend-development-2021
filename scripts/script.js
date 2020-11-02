b5d4bdfc3a012a86eb09571da3619ce887254cc2
Chevelly <57796339+Chevelly@users.noreply.github.com>
2020-11-02 01:57:19 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,52 @@
 // JavaScript Document
 
+
+// ARTICLE SLIDER
+var articleSlides = document.getElementsByClassName("articleSlides");
+var cirkels = document.getElementsByClassName("cirkel");
+var counter = 0;
+
+/* Functie om alle articles te verbergen */
+function verbergArticles() {
+    for (var i = 0; i < articleSlides.length; i++) {
+        articleSlides[i].classList.remove("visible");
+    }
+}
+
+/* Functie om alle rondjes te verwijderen */
+function verwijderRondje() {
+    for (var i = 0; i < articleSlides.length; i++) {
+        cirkels[i].classList.remove("rondje");
+    }
+}
+
+/* Functie voor de enkele article loop en cirkels */
+function articleLoop() {
+    var huidigArticle = articleSlides[counter];
+    var huidigRondje = cirkels[counter];
+    huidigArticle.classList.add("visible");
+    verwijderRondje();
+    huidigRondje.classList.add("rondje");
+    counter++;
+}
+
+/* Functie het sliden van de article */
+function articleSlider() {
+    if (counter < articleSlides.length) {
+        articleLoop();
+    } else {
+        counter = 0;
+        verbergArticles();
+        articleLoop();
+    }
+}
+
+/* Toont eerste article, stelt tijd in en roept de volgende article op */
+setTimeout(articleSlider, 350);
+var articleSlideshowInterval = setInterval(articleSlider, 3500);
+
+
+// NEWSLETTER FORMULIER
 /* drukken met de vinger op signUp */
 var signUp = document.querySelector("footer span");
 
